21a1c13516927020b04407f66de6af742bc26391
umberpiero <58850934+umberpiero@users.noreply.github.com>
2019-12-13 14:30:27 +0100

    Modificaremota

--- GestioneVideo_with_DAO/src/is/gestionevideo/Main.java
+++ GestioneVideo_with_DAO/src/is/gestionevideo/Main.java
@@ -53,7 +53,7 @@ public class Main {
 			VideoApprofondimento v4 = new VideoApprofondimento(nome_evento, data_evento, Sport.CALCIO);
 		
 			v4.addGiornalista(new Giornalista("TIZIO", "CAIO"));
-			v4.addGiornalista(new Giornalista("TOPOLINO", "PLUTO"));
+			v4.addGiornalista(new Giornalista("TOPOLINO", "MODIFICAREMOTA"));
 
 			nome_evento = "Partita Udinese-Napoli";
 

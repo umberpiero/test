7cb1a283719da4102baf628922f4f42978639b31
umber <umber@DESKTOP-3Q8GPJO>
2019-12-13 13:56:22 +0100

    Bug-fix al Main

--- GestioneVideo_with_DAO/src/is/gestionevideo/Main.java
+++ GestioneVideo_with_DAO/src/is/gestionevideo/Main.java
@@ -82,6 +82,7 @@ public class Main {
 			gestionevideo.rimuoviVideo(v2);
 			gestionevideo.rimuoviVideo(v3);
 			gestionevideo.rimuoviVideo(v4);
+			gestionevideo.rimuoviVideo(v5);
 
 		} catch(SQLException e) {
 			System.out.println("Errore accesso DB");

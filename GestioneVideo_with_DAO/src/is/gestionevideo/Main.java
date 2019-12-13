822778ddfe21d9e76a2c626cad7c3a8fd82a93e6
umber <umber@DESKTOP-3Q8GPJO>
2019-12-13 14:15:34 +0100

    Messaggio di terminazione

--- GestioneVideo_with_DAO/src/is/gestionevideo/Main.java
+++ GestioneVideo_with_DAO/src/is/gestionevideo/Main.java
@@ -83,6 +83,8 @@ public class Main {
 			gestionevideo.rimuoviVideo(v3);
 			gestionevideo.rimuoviVideo(v4);
 			gestionevideo.rimuoviVideo(v5);
+			
+			System.out.println("Terminato.");
 
 		} catch(SQLException e) {
 			System.out.println("Errore accesso DB");
